bc6dfd15de4fad83fe81f8930b67834ebe13767a
Jay Phillips <jayphillips@icloud.com>
2021-02-20 16:29:58 -0500

    first commit

---
+++ User Location/AddressLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressLabelView.swift
+//  User Location
+//
+//  Created by Jay Phillips on 2/18/21.
+//
+
+import UIKit
+
+class AddressLabelView: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
